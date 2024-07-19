e21f5f344d73431418ae41ce6719e85062fc8171
EnesBaytekin <enesbaytekin38@gmail.com>
2024-07-19 23:11:10 +0300

    refactor: use string instead of char*

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <cstring>
 
 #define VERSION "1.0.0"
 
+using namespace std; 
+
 void version() {
-    std::cout << "Version: " << VERSION << std::endl;
+    cout << "Version: " << VERSION << endl;
 }
 
 void help() {
-    std::cout << "Usage: tree [OPTIONS] [PATH]" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Options:" << std::endl;
-    std::cout << "    -h, --help               Show this help menu." << std::endl;
-    std::cout << "    -v, --version            Show the version." << std::endl;
-    std::cout << "    -d, --depth <number>     Print tree with the given depth." << std::endl;
-    std::cout << std::endl;
+    cout << "Usage: tree [OPTIONS] [PATH]" << endl;
+    cout << endl;
+    cout << "Options:" << endl;
+    cout << "    -h, --help               Show this help menu." << endl;
+    cout << "    -v, --version            Show the version." << endl;
+    cout << "    -d, --depth <number>     Print tree with the given depth." << endl;
+    cout << endl;
 }
 
-void error(const char* msg, int code) {
-    std::cout << "\033[31mERROR: \033[0m" << msg << std::endl;
+void error(const string msg, int code) {
+    cout << "\033[31mERROR: \033[0m" << msg << endl;
     exit(code);
 }
 
@@ -32,13 +33,13 @@ int main(int argc, char* argv[]) {
     int depth = 0;
 
     for (int i=0; i<argc; ++i) {
-        if (strcmp(argv[i], "--depth") == 0 || strcmp(argv[i], "-d") == 0) {
+        if ((string)argv[i] == "--depth" || (string)argv[i] == "-d") {
             if (argc == i+1) error("A number should come after '-d' tag.\nType 'tree --help' to see the usage.", 1);
             depth = atoi(argv[++i]);
-        } else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0) {
+        } else if ((string)argv[i] == "--version" || (string)argv[i] == "-v") {
             version();
             exit(0);
-        } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+        } else if ((string)argv[i] == "--help" || (string)argv[i] == "-h") {
             help();
             exit(0);
         }
