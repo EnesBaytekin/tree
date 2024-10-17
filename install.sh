f4f18afe154a3782266a1f1c875c84719eeed9b2
EnesBaytekin <enesbaytekin38@gmail.com>
2024-10-17 23:31:03 +0300

    chore: add bash completions

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-sudo wget https://github.com/EnesBaytekin/tree/releases/download/v1.0.1/tree -P /usr/bin
-sudo chmod +x /usr/bin/tree
+if [[ -f /usr/bin/tree ]] then
+else
+    sudo wget https://github.com/EnesBaytekin/tree/releases/download/v1.0.1/tree -P /usr/bin
+    sudo chmod +x /usr/bin/tree
+    sudo cp _tree_completions /usr/share/bash-completion/completions/tree
+fi
